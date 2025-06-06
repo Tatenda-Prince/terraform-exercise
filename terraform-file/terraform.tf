26180a56360c9225f5ad189db1404247f50ba31c
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-06-06 13:02:21 +0200

    updated the terraform file

---
+++ terraform-file/terraform.tf
@@ -0,0 +1,12 @@
+# Declare the required version of Terraform
+terraform {
+  required_version = ">= 1.4.0"
+
+  # Declare the required provider and its version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
